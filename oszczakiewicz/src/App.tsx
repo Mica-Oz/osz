98319f1229ba6cda8d49d8fbc45a2429463b622b
mica-oz <kahulaoz@gmail.com>
2025-06-09 15:02:04 -0700

    stuff

---
+++ oszczakiewicz/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import oz from "../public/oz.jpeg";
+// import viteLogo from '/vite.svg'
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <img src={oz} className="oz" alt="OZ logo" />
+    </>
+  );
+}
+
+export default App;
